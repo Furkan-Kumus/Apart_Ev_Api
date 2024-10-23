a0fb6705d2f5a8934ce237e8bbc05e8f63099999
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2024-10-24 00:58:24 +0300

    some updates

---
+++ ApartEv_Api/src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; private set; }
+
+        [JsonPropertyName("createdDate")]
+        public DateTime CreatedDate { get; private set; }
+
+        [JsonConstructor]
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        public IntegrationEvent(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+    }
+}
