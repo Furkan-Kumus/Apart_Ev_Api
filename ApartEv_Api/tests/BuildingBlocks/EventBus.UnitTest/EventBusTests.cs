32a422a0e29188e8e2df37d90d377d1eccd1990a
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2024-10-25 10:27:37 +0300

    tests start

---
+++ ApartEv_Api/tests/BuildingBlocks/EventBus.UnitTest/EventBusTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection.ServiceCollection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EventBus.UnitTest
+{
+    [TestClass]
+    public class EventBusTests
+    {
+        private ServiceCollection services;
+
+        public EventBusTests(ServiceCollection services)
+        {
+            this.services = services;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ApartEv_Api/tests/BuildingBlocks/EventBus.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventBus.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EventBus.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc530786-7341-438c-9e44-bda90dbc6940")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
